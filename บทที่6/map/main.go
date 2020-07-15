f743a15472ef6830e8a705a3fe60e1992605d526
aapplesoda <aapple.soda@gmail.com>
2020-07-15 18:53:49 +0700

    commit by git terminal 15/07/20

--- บทที่6/map/main.go
+++ บทที่6/map/main.go
@@ -5,5 +5,67 @@ import (
 )
 
 func main() {
-	fmt.Println("H
+	var x map[string]int
+	fmt.Println(x)
+	//
+
+	y := make(map[int]int)
+	y[1] = 10
+	fmt.Println(y[1])
+	//y[1] ไม่ได้หมายถึงสมาชิกตัวที่สองเหมือนarray
+	//mapไม่มีการเรียงลำดีบสมาชิก
+	//indexในmapไม่มีการเรียงลำดับ
+	//indexในmapเป็นstring,intอื่นๆได้
+	//
+
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
+	fmt.Println(elements["Li"])
+	//
+
+	/*
+		elements := map[string]string{
+			"H":  "Hydrogen",
+			"He": "Helium",
+			"Li": "Lithium",
+			"Be": "Beryllium",
+			"B":  "Boron",
+			"C":  "Carbon",
+			"N":  "Nitrogen",
+			"O":  "Oxygen",
+			"F":  "Fluorine",
+			"Ne": "Neon",
+		}//short
+	*/
+
+	elements3 := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+	}
+	fmt.Println(elements3)
+
+	/*
+		๤ังเกตว่าเราเปลี่ยนจากmap[string]stringเป็นmap[string]map[string]stringตอนนี้เรามีแมปของสตริงไปยังแมปของสตริงไปยังสติงแมปที่อยู่ด้าน
+		นอกถูกใ๡้เป็นตารางค้นหาโดยใ๡้๤ัญลักษณ์ของธาตุขณะที่แมปที่อยู่ด้านในถูกใ๡้เพื่อเก็บข้อมูลทั่วไปของธาตุนั้น
+	*/
+
 }
--- บทที่6/slice/main.go
+++ บทที่6/slice/main.go
@@ -35,8 +35,13 @@ func main() {
 
 	slice1 := []int{1, 2, 3}
 	slice2 := append(slice1, 4, 5)
-
 	//append จะรวมsliceเข้าด้วยกัน
+
+	slice1 := []int{1, 2, 3}
+	slice2 := make([]int, 2)
+	copy(slice2, slice1)
+	fmt.Println(slice1, slice2)
+	//copy จะคัดลอกจากslice1 ไปยัง slice2 ตามขนาดความจุ
 
 	fmt.Println(x)
 	fmt.Println(z)
