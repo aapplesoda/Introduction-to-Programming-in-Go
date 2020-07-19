d081f2b4cfb6f8a7f71395f818174bd5a2799258
aapplesoda <aapple.soda@gmail.com>
2020-07-19 16:23:00 +0700

    commit by git terminal

--- บทที่8/main.go
+++ บทที่8/main.go
@@ -1,6 +1,37 @@
 package main
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+>>>>>>> c6aec85... Commit by git terminal.
 //เรื่องpointer
 func main() {
 
 }
+<<<<<<< HEAD
+=======
+=======
+import "fmt"
+
+//เรื่องpointer
+
+func zero(xPtr *int) {
+	*xPtr = 0
+}
+
+func main() {
+	x := 5
+	fmt.Println(&x)
+	//ตำแหน่งaddress
+
+	zero(&x)
+	fmt.Println(x) // x is 0
+}
+
+//& เก็บตำแหน่งaddress
+//* เก็บค่าในaddress
+//new
+//passby value
+//passby pointer
+>>>>>>> bd3fcaa... Commit by git terminal.
+>>>>>>> c6aec85... Commit by git terminal.
